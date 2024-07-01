71f04c8181d19f60f150052d2f5251f99428d59c
fesaaa <59693702+fesaaa@users.noreply.github.com>
2024-07-01 15:34:23 +0700

    remove app controller

--- src/config/master.module.ts
+++ src/config/master.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from '../app.service';
-import { AppController } from '../app.controller';
 import { DatabaseModule } from './database.module';
 
 @Module({
   imports: [
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class MasterModule {}
