d815dd797ca154904ed75d6a4fda375ef31fc21d
fesaaa <59693702+fesaaa@users.noreply.github.com>
2024-07-03 15:15:48 +0700

    member forienkey to role

--- src/models/member.model.ts
+++ src/models/member.model.ts
@@ -1,39 +1,51 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
-import { IMember } from "src/interface/models/member.model";
+import {
+    BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { IMember } from 'src/interface/models/member.model';
+import { Role } from './role.model';
 
 @Table({
-    paranoid: true,
+  paranoid: true,
 })
 export class Member extends Model<Member | IMember> implements IMember {
-    @Column({
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-        type: DataType.INTEGER,
-    })
-    id: number;
+  @Column({
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataType.INTEGER,
+  })
+  id: number;
 
-    @Column({
-        allowNull: false,
-        type: DataType.INTEGER,
-    })
-    projectId: number;
+  @Column({
+    allowNull: false,
+    type: DataType.INTEGER,
+  })
+  projectId: number;
 
-    @Column({
-        allowNull: false,
-        type: DataType.INTEGER,
-    })
-    userId: number;
+  @Column({
+    allowNull: false,
+    type: DataType.INTEGER,
+  })
+  userId: number;
 
-    @Column({
-        allowNull: true,
-        type: DataType.INTEGER,
-    })
-    roleId?: number;
+  @ForeignKey(() => Role)
+  @Column({
+    allowNull: true,
+    type: DataType.INTEGER,
+  })
+  roleId?: number;
 
-    @Column({
-        allowNull: true,
-        type: DataType.INTEGER,
-    })
-    status?: number;
+  @Column({
+    allowNull: true,
+    type: DataType.INTEGER,
+  })
+  status?: number;
+
+  @BelongsTo(() => Role)
+  role?: Role;
 }
--- src/models/role.model.ts
+++ src/models/role.model.ts
@@ -1,40 +1,46 @@
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript';
 import { IRole } from 'src/interface/models/role.model';
+import { Member } from './member.model';
 
 @Table({
   paranoid: true,
 })
 export class Role extends Model<Role | IRole> implements IRole {
-    @Column({
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-        type: DataType.INTEGER,
-    })
-    id: number;
+  @Column({
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataType.INTEGER,
+  })
+  id: number;
 
-    @Column({
-        allowNull: false,
-        type: DataType.STRING,
-    })
-    name: string;
+  @Column({
+    allowNull: false,
+    type: DataType.STRING,
+  })
+  name: string;
 
-    @Column({
-        allowNull: true,
-        type: DataType.BOOLEAN,
-    })
-    isEditPlan?: boolean;
+  @Column({
+    allowNull: true,
+    type: DataType.BOOLEAN,
+  })
+  isEditPlan?: boolean;
 
-    @Column({
-        allowNull: true,
-        type: DataType.BOOLEAN,
-    })
-    isEditProject?: boolean;
+  @Column({
+    allowNull: true,
+    type: DataType.BOOLEAN,
+  })
+  isEditProject?: boolean;
 
-    @Column({
-        allowNull: true,
-        type: DataType.BOOLEAN,
-    })
-    isEditTask?: boolean;
+  @Column({
+    allowNull: true,
+    type: DataType.BOOLEAN,
+  })
+  isEditTask?: boolean;
+
+  @HasMany(() => Member, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  members?: Member[];
 }
-
