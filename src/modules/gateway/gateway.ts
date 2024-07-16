a1db97aabf1ad69e78ba44ec614c40f7c78d2043
VirapatB <virapat2003@hotmail.com>
2024-07-16 09:38:11 +0700

    cors

--- src/modules/gateway/gateway.ts
+++ src/modules/gateway/gateway.ts
@@ -1,54 +1,76 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { InjectModel } from "@nestjs/sequelize";
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from "socket.io";
-import { User } from "src/models/user.model";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { User } from 'src/models/user.model';
 
 @Injectable()
 @WebSocketGateway({
-    cors: {
-        origin: ['*'],
-    },
+  cors: {
+    origin: ['http://localhost:5173'],
+  },
 })
 export class Gateway implements OnModuleInit {
-    @WebSocketServer()
-    server: Server
+  @WebSocketServer()
+  server: Server;
 
-    constructor(@InjectModel(User) private repository: typeof User) {}
+  constructor(@InjectModel(User) private repository: typeof User) {}
 
-    onModuleInit() {
-        this.server.on('connection', (socket) => {
-            socket.on('register', async (userId: number) => {
-                try {
-                    await this.repository.update({ socketId: socket.id }, { where: { id: Number(userId) }});
-                } catch (err) {
-                    throw new Error(err);
-                }
-            });
-
-            socket.on('disconnect', async () => {
-                try {
-                    const user = await this.repository.findOne({ where: { socketId: socket.id } });
-                    if (user) {
-                        await this.repository.update({ socketId: null }, {where: { id: user.id }});
-                        console.log(`User ${user.id} disconnected`);
-                    }
-                } catch (err) {
-                    throw new Error(err);
-                }
-            });
-        });
-    }
-
-    @SubscribeMessage('invite')
-    async handleInviteEvent(@MessageBody() userId: number) {
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      socket.on('register', async (userId: number) => {
         try {
-            const receiver = await this.repository.findByPk(Number(userId));
-            if (receiver && receiver.socketId) {
-                this.server.to(receiver.socketId).emit('newInvite', "new Invite");
-            }
+          await this.repository.update(
+            { socketId: socket.id },
+            { where: { id: Number(userId) } },
+          );
         } catch (err) {
-            throw new Error(err);
+          throw new Error(err);
         }
+      });
+
+      socket.on('disconnect', async () => {
+        try {
+          const user = await this.repository.findOne({
+            where: { socketId: socket.id },
+          });
+          if (user) {
+            await this.repository.update(
+              { socketId: null },
+              { where: { id: user.id } },
+            );
+            console.log(`User ${user.id} disconnected`);
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      });
+    });
+  }
+
+  @SubscribeMessage('invite')
+  async handleInviteEvent(
+    @MessageBody()
+    inviteDetail: {
+      userId: number;
+      projectId: number;
+      roleId: number;
+    },
+  ) {
+    try {
+      const receiver = await this.repository.findByPk(
+        Number(inviteDetail.userId),
+      );
+      if (receiver && receiver.socketId) {
+        this.server.to(receiver.socketId).emit('newInvite', inviteDetail);
+      }
+    } catch (err) {
+      throw new Error(err);
     }
-} 
+  }
+}
