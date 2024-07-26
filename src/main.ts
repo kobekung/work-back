7862807a60f62b94dc4384645e4b9899481cba15
fesaaa <59693702+fesaaa@users.noreply.github.com>
2024-07-26 14:10:30 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //allow CORS localhost:5173
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://pmms.rtarf.mi.th/v2'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
