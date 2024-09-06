9d3d568a5017fb6d4c5fa4eafdbd136ca450db81
fesaaa <59693702+fesaaa@users.noreply.github.com>
2024-09-06 15:11:24 +0700

    chore: Add UserModule to CommentModule imports

--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -5,9 +5,14 @@ import { Task } from 'src/models/task.model';
 import { User } from 'src/models/user.model';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { Comment } from 'src/models/comment.model';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Comment, Member, Task, User])],
+  imports: [
+    SequelizeModule.forFeature([Comment, Member, Task, User]),
+    UserModule,
+  ],
   controllers: [CommentController],
   providers: [CommentService],
   exports: [CommentService],
