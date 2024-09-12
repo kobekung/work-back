effb87000093b235335bcc45908c6e16438c36c2
fesaaa <59693702+fesaaa@users.noreply.github.com>
2024-09-12 09:14:13 +0700

    allow

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,11 @@ async function bootstrap() {
       'http://localhost:5173/v2',
       'http://localhost:5173/v2/',
       'https://pmms.rtarf.mi.th/v2',
+      'https://pmms.rtarf.mi.th/v2/',
       'http://localhost:5500',
       'localhost:5173'
     ],
-    methods: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
